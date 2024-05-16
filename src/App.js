fa95c38e84bb229322577b3a256ea8797f73cc7b
irtaza777 <irtazarizvi7@gmail.com>
2024-05-16 12:13:41 +0500

    redux todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Addtodo from './components/Addtodo';
+import Todolist from './components/Todolist';
+
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>React-redux toolkit</h1>
+     <Addtodo/>
+     <Todolist/>
     </div>
   );
 }
